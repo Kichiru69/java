3bcbe6677bacb525d5348815b001d2f7354e8681
Kichiru69 <49789810+Kichiru69@users.noreply.github.com>
2021-04-25 22:36:57 +0300

    После 3.3

---
+++ Sandbox/src/test/java/ru/stqa/pft/sandbox/EquationTest.java
@@ -0,0 +1,34 @@
+package ru.stqa.pft.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class EquationTest {
+  @Test
+  public void test0() {
+    Equation e = new Equation(1, 1, 1);
+    Assert.assertEquals(e.rootNumber(), 0);
+  }
+  @Test
+  public void test1() {
+    Equation e = new Equation(1, 2, 1);
+    Assert.assertEquals(e.rootNumber(), 1);
+  }
+  @Test
+  public void testLinear() {
+    Equation e = new Equation(0, 1, 1);
+    Assert.assertEquals(e.rootNumber(), 1);
+  }
+
+  @Test
+  public void testConstant() {
+    Equation e = new Equation(0, 0, 1);
+    Assert.assertEquals(e.rootNumber(), 0);
+  }
+
+  @Test
+  public void testZero() {
+    Equation e = new Equation(0, 0, 0);
+    Assert.assertEquals(e.rootNumber(), -1);
+  }
+}
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,18 +1,33 @@
 package ru.stqa.pft.addressbook.appmanager;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
-  FirefoxDriver wd;
+  WebDriver wd;
   private ContactHelper contactHelper;
   private NavigationHelper navigationHelper;
   private GroupHelper groupHelper;
   private SessionHelper sessionHelper;
+  private String browser;
+
+  public ApplicationManager(String browser) {
+    this.browser = browser;
+  }
 
   public void init() {
-    wd = new FirefoxDriver();
+    if (browser == BrowserType.FIREFOX) {
+      wd = new FirefoxDriver();
+    } else if (browser == BrowserType.CHROME) {
+      wd = new ChromeDriver();
+    } else if (browser == BrowserType.IE) {
+      wd = new InternetExplorerDriver();
+    }
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     wd.get("http://localhost/addressbook/group.php");
     groupHelper = new GroupHelper(wd);
