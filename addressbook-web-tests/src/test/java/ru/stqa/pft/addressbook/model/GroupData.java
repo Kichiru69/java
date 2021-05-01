11e2382fda59b903779105b1b2ced309f43f542e
Kichiru69 <49789810+Kichiru69@users.noreply.github.com>
2021-05-01 22:16:51 +0300

    4.10

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -17,7 +17,7 @@ public class GroupData {
   }
 
   public GroupData(String name, String header, String footer) {
-    this.id = 0;
+    this.id = Integer.MAX_VALUE;
     this.name = name;
     this.header = header;
     this.footer = footer;
@@ -45,21 +45,21 @@ public class GroupData {
             '}';
   }
 
+  public void setId(int id) {
+    this.id = id;
+  }
+
   @Override
   public boolean equals(Object o) {
     if (this == o) return true;
     if (o == null || getClass() != o.getClass()) return false;
     GroupData groupData = (GroupData) o;
-    return id == groupData.id && Objects.equals(name, groupData.name);
+    return Objects.equals(name, groupData.name);
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(id, name);
-  }
-
-  public void setId(int id) {
-    this.id = id;
+    return Objects.hash(name);
   }
 
 }
