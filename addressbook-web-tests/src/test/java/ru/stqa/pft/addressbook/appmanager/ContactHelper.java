59d31a84dece0b6253b990f32a2d8e7df53166e6
Kichiru69 <49789810+Kichiru69@users.noreply.github.com>
2021-05-02 00:41:59 +0300

    Считает при удалении

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -3,10 +3,14 @@ package ru.stqa.pft.addressbook.appmanager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import ru.stqa.pft.addressbook.model.ContactData;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ContactHelper extends BaseHelper {
 
@@ -69,5 +73,18 @@ public class ContactHelper extends BaseHelper {
 
   public boolean isThereAContact() {
      return isElementPresent(By.name("selected[]"));
+  }
+
+  public List<ContactData> getContactsList() {
+    List<ContactData> contacts = new ArrayList<ContactData>();
+    List<WebElement> elements = wd.findElements(By.name("selected[]"));
+    for (WebElement element : elements) {
+      String firstname = element.getText();
+      ContactData contact = new ContactData(firstname, null,
+              null, null, null, null);
+      contacts.add(contact);
+    }
+    return contacts;
+
   }
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
@@ -1,7 +1,11 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ContactDeletionTests extends TestBase{
 
@@ -13,10 +17,13 @@ public class ContactDeletionTests extends TestBase{
       app.getContactHelper().createContact(new ContactData("Elena", "Alfutova",
               "Moscow, street Testovaya 77, 88", "849566655588", "test@test.ru", "test1"), true);
     }
+    List<ContactData> before = app.getContactHelper().getContactsList();
     app.getContactHelper().selectContact();
     app.getContactHelper().contactDeletion();
     app.getContactHelper().alertAccept();
     app.getNavigationHelper().returnToHomePage();
+    List<ContactData> after = app.getContactHelper().getContactsList();
+    Assert.assertEquals(after.size(), before.size()- 1);
 
   }
 }
