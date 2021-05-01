e7fff736203d70a51dec3ed63a2d98c906b95764
Kichiru69 <49789810+Kichiru69@users.noreply.github.com>
2021-05-01 21:54:50 +0300

    4.10

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -4,12 +4,12 @@ import java.util.Objects;
 
 public class GroupData {
 
-  private final String id;
+  private int id;
   private final String name;
   private final String header;
   private final String footer;
 
-  public GroupData(String id, String name, String header, String footer) {
+  public GroupData(int id, String name, String header, String footer) {
     this.id = id;
     this.name = name;
     this.header = header;
@@ -17,7 +17,7 @@ public class GroupData {
   }
 
   public GroupData(String name, String header, String footer) {
-    this.id = null;
+    this.id = 0;
     this.name = name;
     this.header = header;
     this.footer = footer;
@@ -35,7 +35,7 @@ public class GroupData {
     return footer;
   }
 
-  public String getId() {  return id;  }
+  public int getId() {  return id;  }
 
   @Override
   public String toString() {
@@ -50,11 +50,16 @@ public class GroupData {
     if (this == o) return true;
     if (o == null || getClass() != o.getClass()) return false;
     GroupData groupData = (GroupData) o;
-    return Objects.equals(id, groupData.id) && Objects.equals(name, groupData.name);
+    return id == groupData.id && Objects.equals(name, groupData.name);
   }
 
   @Override
   public int hashCode() {
     return Objects.hash(id, name);
   }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
 }
