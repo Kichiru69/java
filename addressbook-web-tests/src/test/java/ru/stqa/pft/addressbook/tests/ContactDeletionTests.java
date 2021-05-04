0f8e8b3920079b66a2cb9de9e580dc39a3e9cb20
Kichiru69 <49789810+Kichiru69@users.noreply.github.com>
2021-05-04 20:29:24 +0300

    Доработки метода удаления контактов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 
+import java.util.Comparator;
 import java.util.List;
 
 public class ContactDeletionTests extends TestBase{
@@ -23,6 +24,12 @@ public class ContactDeletionTests extends TestBase{
     app.getNavigationHelper().returnToHomePage();
     List<ContactData> after = app.getContactHelper().getContactsList();
     Assert.assertEquals(after.size(), before.size()- 1);
+
+    before.remove(before.size() - 1);
+    Comparator<? super ContactData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
+    before.sort(byId);
+    after.sort(byId);
+    Assert.assertEquals(before, after);
 
   }
 }
