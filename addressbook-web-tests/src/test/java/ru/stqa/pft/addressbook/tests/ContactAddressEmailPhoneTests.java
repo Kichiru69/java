bc56c365d388e3040f03c466033a0f372528dce8
Kichiru69 <49789810+Kichiru69@users.noreply.github.com>
2021-05-17 00:56:50 +0300

    Вместо трех классов по сравнению главной страницы и редактирования все в одном

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactAddressEmailPhoneTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactAddressEmailPhoneTests.java
@@ -9,24 +9,38 @@ import java.util.stream.Collectors;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-public class ContactPhoneTests extends TestBase{
+public class ContactAddressEmailPhoneTests extends TestBase {
   @Test
 
-  public void testContactPhone () {
+  public void testContactAddressEmailPhone() {
     app.goTo().HomePage();
     ContactData contact = app.contact().all().iterator().next();
     ContactData contactInfoFromEditForm = app.contact().infoFromEditForm(contact);
+    assertThat(contact.getAllEmails(), equalTo(mergeEmails(contactInfoFromEditForm)));
+    assertThat(contact.getAddress(), equalTo(contactInfoFromEditForm.getAddress()));
     assertThat(contact.getAllPhones(), equalTo(mergePhones(contactInfoFromEditForm)));
+  }
+
+  private String mergeEmails(ContactData contact) {
+    return Arrays.asList(contact.getEmail(), contact.getEmail2(), contact.getEmail3())
+            .stream().filter((s) -> ! s.equals(""))
+            .map(ContactAddressEmailPhoneTests::cleanedEmail)
+            .collect(Collectors.joining("\n"));
   }
 
   private String mergePhones(ContactData contact) {
     return Arrays.asList(contact.getHomePhone(), contact.getMobilePhone(), contact.getWorkPhone())
             .stream().filter((s) -> ! s.equals(""))
-            .map(ContactPhoneTests::cleaned)
+            .map(ContactAddressEmailPhoneTests::cleanedPhone)
             .collect(Collectors.joining("\n"));
   }
 
-  public static String cleaned(String phone) {
+  public static String cleanedPhone(String phone) {
     return phone.replaceAll("\\s", "").replaceAll("[-()]", "");
   }
+
+  public static String cleanedEmail(String email) {
+    return email.replaceAll("\\s", "");
+  }
 }
+
