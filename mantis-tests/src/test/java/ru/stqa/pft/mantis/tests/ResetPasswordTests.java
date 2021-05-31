2bcb994d8c04cef4dfe401e8b239261cc32d7fb5
Kichiru69 <49789810+Kichiru69@users.noreply.github.com>
2021-06-01 01:29:16 +0300

    наброски

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/ResetPasswordTests.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/ResetPasswordTests.java
@@ -9,6 +9,8 @@ import org.testng.annotations.Test;
 import ru.lanwen.verbalregex.VerbalExpression;
 import ru.stqa.pft.mantis.model.MailMessage;
 
+import javax.mail.MessagingException;
+import java.io.IOException;
 import java.util.List;
 
 import static org.testng.Assert.assertTrue;
@@ -17,8 +19,8 @@ public class ResetPasswordTests extends TestBase{
 
 
     @Test
-  public void resetPasswordTest() throws InterruptedException {
-      app.db().contacts();
+  public void resetPasswordTest() throws InterruptedException, MessagingException, IOException {
+      //app.db().contacts();
 
       app.resetPassword().loginAsAdmin();
       app.resetPassword().initChooseMenu(6);
@@ -26,6 +28,20 @@ public class ResetPasswordTests extends TestBase{
       app.resetPassword().initChooseUserById(2);
       app.resetPassword().initResetPassword();
 
-
+      String email = "ealfutova@localhost.localdomein";
+      String user = "Elena";
+      String password = "password";
+      //List<MailMessage> mailMessages = app.mail().waitForMail(2, 10000);
+      List<MailMessage> mailMessages = app.james().waitForMail(user, password, 60000);
+      String confirmationLink = findConfirmationLink(mailMessages, email);
+      app.registration().finish(confirmationLink, password);
+      assertTrue(app.newSession().login(user, password));
     }
+
+  private String findConfirmationLink(List<MailMessage> mailMessages, String email) {
+    MailMessage mailMessage = mailMessages.stream().filter((m) -> m.to.equals(email)).findFirst().get();
+    VerbalExpression regex = VerbalExpression.regex().find("http://").nonSpace().oneOrMore().build();
+    return regex.getText(mailMessage.text);
+  }
+
 }
