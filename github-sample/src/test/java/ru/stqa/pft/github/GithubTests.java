91f0151106bcc954c5c98ff3f281d380b8257139
Kichiru69 <49789810+Kichiru69@users.noreply.github.com>
2021-06-12 14:29:31 +0300

    После домашки

---
+++ github-sample/src/test/java/ru/stqa/pft/github/GithubTests.java
@@ -0,0 +1,19 @@
+package ru.stqa.pft.github;
+
+import com.google.common.collect.ImmutableMap;
+import com.jcabi.github.*;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class GithubTests {
+
+  @Test
+  public void testCommits() throws IOException {
+    Github github = new RtGithub("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 ");
+    RepoCommits commits = github.repos().get(new Coordinates.Simple("Kichiru69", "java")).commits();
+    for (RepoCommit commit : commits.iterate(new ImmutableMap.Builder<String, String>().build())) {
+      System.out.println(new RepoCommit.Smart(commit).message());
+    }
+  }
+}
